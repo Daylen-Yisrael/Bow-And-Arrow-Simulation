4c2b0193df1913241decdfa3a8f3e29fb0646854
Yeeker <violentanime@gmail.com>
2022-05-21 21:26:37 -0500

    Save

--- Refresher/Assets/Scripts/Player.cs
+++ Refresher/Assets/Scripts/Player.cs
@@ -3,9 +3,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float jumpVelocity = 2;
+    Rigidbody _rb;
+    bool _isJumping;
     void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
-    
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _isJumping = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_isJumping)
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            _isJumping = false;
+        }
+    }
 }    
